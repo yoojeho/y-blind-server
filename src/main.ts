67b0ad2140c87177d6ba1c32febb7339476ab11a
yoojeho <jeho328@naver.com>
2025-10-17 16:48:19 +0900

    feat: 스웨거 문서에서 dto 모음 제거

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  document.components = { schemas: {} };
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(3000);
