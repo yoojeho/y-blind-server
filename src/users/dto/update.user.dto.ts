b7d5e7a410851afdacc7b25c6672e32f1e070a72
Taekwon Yoo <takun617@gmail.com>
2025-10-29 20:29:51 +0900

    feat: ✨ user update (nickname)

--- src/users/dto/update.user.dto.ts
+++ src/users/dto/update.user.dto.ts
@@ -1,13 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString, Matches } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
-  @ApiProperty({ example: "user01", required: false })
-  @IsOptional()
-  @IsString()
-  @Matches(/^[a-zA-Z0-9_-]{3,32}$/)
-  username?: string;
-
   @ApiProperty({ example: "홍길동", required: false })
   @IsOptional()
   @IsString()
